f67e7dc8e0e2a450428cefd2749ae08c9601b2b4
marvelmikel <marvelmikel1997@gmail.com>
2024-01-14 02:44:46 +0000

    delete dept api integrated

--- src/components/ListDepartmentComponent.jsx
+++ src/components/ListDepartmentComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
-import { getAllDepartments } from '../services/DepartmentService';
+import { deleteDepartment, getAllDepartments } from '../services/DepartmentService';
 import {Link} from 'react-router-dom';
 
 const ListDepartmentComponent = () => {
@@ -10,16 +10,30 @@ const ListDepartmentComponent = () => {
     const navigator = useNavigate();
 
     useEffect(() => {
+        listOfDepartments();
+    }, []) 
+
+    function listOfDepartments(){
         getAllDepartments().then((response) => {
             console.log(response.data);
             setDepartments(response.data);
         }).catch(error => {
             console.error(error);
         })
-    }, []) 
+
+    }
 
     function updateDepartment(id){
         navigator(`/edit-department/${id}`)
+    }
+
+    function removeDepartment(id){
+        deleteDepartment(id).then((response) => {
+            console.log(response.data);
+            listOfDepartments();
+        }).catch(error => {
+            console.error(error);
+        })
     }
   return (
     <div className='container'>
@@ -42,7 +56,9 @@ const ListDepartmentComponent = () => {
                             <td>{department.departmentName}</td>
                             <td>{department.departmentDescription}</td>
                             <td>
-                                <button onClick={() => updateDepartment(department.id)} className='btn btn-info'> Update</button>
+                               
+                            <button onClick={() => updateDepartment(department.id)} className='btn btn-info'> Update</button>
+                            <button onClick={() => removeDepartment(department.id)} className='btn btn-danger' style={{marginLeft:"10px"}}> Delete</button>
                             </td>
                         </tr>
                         )
--- src/services/DepartmentService.js
+++ src/services/DepartmentService.js
@@ -9,3 +9,5 @@ export const createDepartment = (department) => axios.post(DEPARTMENT_REST_API_B
 export const getDepartmentById = (departmentId) => axios.get(DEPARTMENT_REST_API_BASE_URL + '/' + departmentId);
 
 export const updateDepartment = (departmentId, department) => axios.put(DEPARTMENT_REST_API_BASE_URL + '/' + departmentId, department); 
+
+export const deleteDepartment = (departmentId) => axios.delete(DEPARTMENT_REST_API_BASE_URL + '/' + departmentId);
